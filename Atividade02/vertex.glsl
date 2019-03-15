69acbba49fad7c45cc38f02333a7e500f493a95d
CELSO ALINERI <celso.alineri@senacsp.edu.br>
2019-03-14 21:15:21 -0300

    Recuperado aula04 e criado pasta da atividade 02

---
+++ Atividade02/vertex.glsl
@@ -0,0 +1,13 @@
+precision highp float;
+
+attribute vec2 position;
+
+varying vec2 vertexPos;
+
+uniform float aspect;
+
+void main(){
+    vertexPos = position;
+    //                  X   Y   Z   W
+    gl_Position = vec4(position.x / aspect, position.y, 0.0, 1.0);
+}
