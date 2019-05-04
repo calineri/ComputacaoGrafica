903d3acc82ee1f1f0363ecb0a3625472cfa60d8c
CELSO ALINERI <celso.alineri@senacsp.edu.br>
2019-05-03 21:54:49 -0300

    criado aula 07

---
+++ Aula07/fragment.glsl
@@ -0,0 +1,13 @@
+precision highp float;
+
+varying vec4 vertexPos;
+
+uniform vec3 color;
+
+void main(){
+    vec4 light = vec4(10.0,10.0,10.0,1.0);
+    
+    float c = distance(light, vertexPos) / 4.0;
+    vec3 shade = color * c;
+    gl_FragColor = vec4(shade,1.0);
+}
